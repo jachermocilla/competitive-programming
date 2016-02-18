54fa420d40d7e6057e7542dd82e34e6839cd861f
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2016-02-18 22:05:38 +0800

    added codechef

---
+++ codechef/SUBINC.cpp
@@ -0,0 +1,32 @@
+/*
+ * Author: jachermocilla@gmail.com
+ * URL: 
+ */
+
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+#define ull unsigned long long
+
+
+int main(){
+    ull T,N,a;
+    ull i,j,ans;
+    ull A[100];
+
+    cin >> T;
+    //cout << T << endl;
+    for (j=0;j<T;j++){
+        cin >> N;
+        for (i=0;i<N;i++){
+            cin >> A[i];
+        }
+/*
+        for (i=0;i<N;i++){
+            cout << A[i];
+        }
+*/
+        cout << endl;
+    }   
+}
