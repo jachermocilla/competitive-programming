76efa6d3b9824d2d11ebe6950894d9a25260c80d
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2016-02-23 22:13:34 +0800

    does not work yet

---
+++ codechef/RECTSQ.cpp
@@ -0,0 +1,40 @@
+/**
+ * author: jachermocilla@gmail.com
+ * url: 
+*/
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+#define ull unsigned long long
+
+int main(){
+    ull i=0,j=0,k=0;
+
+    ull T,N,M,min,max,lcd;
+
+    cin >> T;
+    for (i=0;i<T;i++){
+        cin >> N >> M;
+        min=(N<=M)?N:M;
+        max=(N>=M)?N:M;
+        lcd=1;
+        for (j=min;j>1;j--){
+            if (((N%j)==0) && ((M%j)==0)){
+                lcd=j;    
+                //break; 
+           }
+        }
+        cout << "lcd:" << lcd << endl;    
+        if (lcd == 1){
+            //cout << "lcd:" << lcd << endl;    
+            cout << (max/min)  << endl;
+        }else{
+            //cout << "lcd:" << lcd << endl;    
+            cout << ((N/lcd)*(M/lcd)) << endl;
+        }
+    }
+    return 0;
+}
---
+++ codechef/template.cpp
@@ -0,0 +1,17 @@
+/**
+ * author: jachermocilla@gmail.com
+ * url: 
+*/
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+#define ull unsigned long long
+
+
+int main(){
+    ull n,i,j,k;
+    return 0;
+}
