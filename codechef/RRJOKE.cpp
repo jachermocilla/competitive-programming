65a9c64145e3dc9b9886aba61de30cdb2e258360
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2016-03-01 21:18:55 +0800

    RRJOKE

--- codechef/RRJOKE.cpp
+++ codechef/RRJOKE.cpp
@@ -11,15 +11,23 @@ using namespace std;
 
 #define ull unsigned long long
 
-
 int main(){
     ull n,i,j,k;
     ull T,N,M;
+    ull PX,PY;
+    ull H;
 
     cin >> T;
 
     for (i=0;i<T;i++){
-        
+        cin >> N;
+
+        H=0;
+        for(j=1;j<=N;j++){            
+            cin >> PX >> PY;
+            H=H^j;
+        }
+        cout << H << endl;
     }
 
     return 0;
