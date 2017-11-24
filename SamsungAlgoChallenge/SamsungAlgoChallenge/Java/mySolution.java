fb64c853d302133b3690e55c52b0381ba4903612
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2017-11-24 15:57:40 +0800

    added SamsungAlgoChallenge

---
+++ SamsungAlgoChallenge/SamsungAlgoChallenge/Java/mySolution.java
@@ -0,0 +1,15 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Please, implement your algorithm from this section.
+//	DO NOT CHANGE THE FUNCTION SIGNATURE
+//	You may add additional methods and class variables
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+ 
+public class mySolution {
+	
+	
+	public int mysolution(int N, int H1row, int H1col, int H2row, int H2col, int T1row, int T1col, int T2row, int T2col, int obs[])	{
+		return -1;
+	}
+}
