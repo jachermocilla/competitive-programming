426a3d6587c6ad4d3bc4e034395919cd0ed47d9c
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2017-11-24 21:33:27 +0800

    edit

--- SamsungAlgoChallenge/Java/mySolution.java
+++ SamsungAlgoChallenge/Java/mySolution.java
@@ -6,6 +6,8 @@
 //
 /////////////////////////////////////////////////////////////////////////////////////////////
 
+//https://stackoverflow.com/questions/22642902/how-to-implement-bfs-on-a-matrix
+
 import java.util.*;
  
 public class mySolution {
@@ -19,9 +21,11 @@ public class mySolution {
         boolean visited;
         List<Node> neighbours;
  
-        Node(int data)
+        Node(int data,int row, int col)
         {
             this.data=data;
+            this.row=row;
+            this.col=col;
             this.neighbours=new ArrayList<>();
  
         }
@@ -42,8 +46,9 @@ public class mySolution {
     }
 
 	
-    public void bfs(Node node)
+    public int bfs(Node node,int N, int H1row, int H1col, int H2row, int H2col, int T1row, int T1col, int T2row, int T2col, int obs[])	
     {
+        int d=0;
         queue.add(node);
         node.visited=true;
         while (!queue.isEmpty())
@@ -51,6 +56,16 @@ public class mySolution {
  
             Node element=queue.remove();
             System.out.print(element.data + "\t");
+            
+            if (node.row==T1row && node.col==T1col){
+                d++;
+                return d; 
+            }
+
+            //TODO: insert 
+            //code to add the neighbors here
+            //////
+
             List<Node> neighbours=element.getNeighbours();
             for (int i = 0; i < neighbours.size(); i++) {
                 Node n=neighbours.get(i);
@@ -63,6 +78,7 @@ public class mySolution {
             }
  
         }
+        return -1;
     }
 
 	
@@ -77,14 +93,13 @@ public class mySolution {
         System.out.println(T1col);
         System.out.println(T2row);
         System.out.println(T2col);
-*/
-        Node node40 =new Node(40);
-        Node node10 =new Node(10);
-        Node node20 =new Node(20);
-        Node node30 =new Node(30);
-        Node node60 =new Node(60);
-        Node node50 =new Node(50);
-        Node node70 =new Node(70);
+        Node node40 =new Node(40,1,1);
+        Node node10 =new Node(10,1,1);
+        Node node20 =new Node(20,1,1);
+        Node node30 =new Node(30,1,1);
+        Node node60 =new Node(60,1,1);
+        Node node50 =new Node(50,1,1);
+        Node node70 =new Node(70,1,1);
  
         node40.addneighbours(node10);
         node40.addneighbours(node20);
@@ -96,10 +111,11 @@ public class mySolution {
         node30.addneighbours(node60);
         node60.addneighbours(node70);
         node50.addneighbours(node70);
+*/
+        Node H1 = new Node(1,H1row,H1col);
         System.out.println("The BFS traversal of the graph is ");
         mySolution bfsExample = new mySolution();
-        bfsExample.bfs(node40);
-
+        bfsExample.bfs(H1,N,H1row,H1col,H2row,H2col,T1row,T1col,T2row,T2col,obs);
 		return -1;
 	}
 }
