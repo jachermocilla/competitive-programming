884151c0970d057f86597770bdd7970357931f17
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2017-12-30 11:43:01 +0800

    edits

---
+++ notebook/graphs/FLOYDWARSHALL.java
@@ -0,0 +1,86 @@
+// A Java program for Floyd Warshall All Pairs Shortest
+// Path algorithm.
+//https://www.geeksforgeeks.org/dynamic-programming-set-16-floyd-warshall-algorithm/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+ 
+class FLOYDWARSHALL
+{
+   final static int INF = 99999; 
+   int n;
+   int g[][];
+   int d[][];
+
+   public FLOYDWARSHALL(int n){
+      this.n=n;
+      g = new int[n][n];
+      d = new int[n][n];
+      for (int i = 0; i < n; i++){
+         for (int j = 0; j < n; j++){
+            g[i][j]=INF;
+            d[i][j]=INF;
+         }
+      }
+   }
+
+   public FLOYDWARSHALL(int g[][]){
+      this(g[0].length);
+      for (int u=0;u<4;u++){
+         for (int v=0;v<4;v++){
+            this.add(u,v,g[u][v]);
+         }
+      }
+   }
+
+   public void add(int u,int v, int w){
+      g[u][v]=w;
+      d[u][v]=w; 
+   }
+
+
+   public void floydWarshall(){
+      /* 
+      for (i = 0; i < V; i++)
+         for (j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+      */
+
+      for (int k = 0; k < n; k++){
+         for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+               if (d[i][k] + d[k][j] < d[i][j]){
+                  d[i][j] = d[i][k] + d[k][j];
+               }
+            }
+         }
+      }
+   }
+ 
+
+   void printSolution(){
+      for (int i=0; i<n; ++i){
+         for (int j=0; j<n; ++j){
+                if (d[i][j]==INF)
+                    System.out.print("INF ");
+                else
+                    System.out.print(d[i][j]+"   ");
+         }
+         System.out.println();
+      }
+   }
+   // Driver program to test above function
+   public static void main (String[] args){
+      int graph[][] =   { {0,   5,  INF, 10},
+                        {INF, 0,   3, INF},
+                        {INF, INF, 0,   1},
+                        {INF, INF, INF, 0}
+                        };
+      
+      FLOYDWARSHALL a = new FLOYDWARSHALL(graph);
+      a.floydWarshall();
+      a.printSolution(); 
+   }
+}
