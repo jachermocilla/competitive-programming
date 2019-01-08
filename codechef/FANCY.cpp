6dd70fd7fec4d55856099a87932298eb490376b5
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2019-01-08 21:45:01 +0800

    FANCY

---
+++ codechef/FANCY.cpp
@@ -0,0 +1,44 @@
+/**
+ * vim: set cindent:
+ * author: jachermocilla@gmail.com
+ * url:  
+*/
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+#define ull unsigned long long
+
+
+int main(){
+   ull n,i,j,k,t;
+   ull T,N,M;
+   string s1;
+   string s2=" not ";   
+   string s3="not ";   
+   string s4=" not";   
+
+   cin >> T;
+   //cout << T;
+   getline(cin,s1);
+   for (t=0;t<T;t++){
+      getline(cin,s1);
+      //cout << s1 << "\n";
+      if (s1.find(s2) != std::string::npos){
+         cout << "Real Fancy 1" << '\n';
+      }
+      else if (s1.find(s3) == 0){
+         cout << "Real Fancy 2" << '\n';
+      }
+      else if (s1.find(s4) == s1.length()-4){
+         cout << "Real Fancy 3" << '\n';
+      }
+      else{
+         cout << "regularly fancy" << '\n';
+      }    
+   }
+
+   return 0;
+}
---
+++ codechef/MONSTER.cpp
@@ -0,0 +1,27 @@
+/**
+ * vim: set cindent:
+ * author: jachermocilla@gmail.com
+ * url: 
+*/
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+#define ull unsigned long long
+
+
+int main(){
+    ull n,i,j,k,t;
+    ull T,N,M;
+    int life[9999999
+
+    cin >> T;
+
+    for (t=0;t<T;t++){
+        
+    }
+
+    return 0;
+}
