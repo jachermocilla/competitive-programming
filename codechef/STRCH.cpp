5768897e0ac810a3f495cffdff2790b521f0210a
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2019-04-09 14:21:30 +0800

    edits

--- codechef/STRCH.cpp
+++ codechef/STRCH.cpp
@@ -34,27 +34,45 @@ int main(){
          cout << "0" << endl;
          continue;
       }
+      //cout << "pos:" << pos << endl;
 
       k=0;
 
       for (ull len=1; len <= N; len++){
          for (i = 0; i <= N - len; i++){
             j=i+len-1;
+            //cout << "start: " << s.at(i) << endl;
+            //cout << "end: " << s.at(j) << endl;
+            if ( i <= pos && j >= pos)
+               k++;
+            else if (s.at(i) == ch || s.at(j) == ch) 
+               k++;
+/*
+
+            int found=0;
             for (m=i;m<=j;m++){ 
-               //cout << s.at(m);
-               if (s.at(m)==ch){
+               cout << s.at(m);
+               if (s.at(m)==ch && !found){
                   k++;
-                  break;
+                  found=true;
+                  continue;
                }
             }
-
-            //cout << endl;
+            cout << endl;
+*/
          }
       }
-   
-      //cout << N*(N+1)/2 << endl;
       cout << k << endl;
-   }
 
+      /*
+      cout << "----------" << endl;
+      //cout << N*(N+1)/2 << endl;
+      for (i=1;i<=N;i++){
+         k=i;
+         cout << (N-k+1) << endl;
+      }
+
+*/
+   }
    return 0;
 }
