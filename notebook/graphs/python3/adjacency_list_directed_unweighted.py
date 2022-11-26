b35d355df955a3089bb3b2f3305db9b9a95589e8
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2022-11-26 15:07:51 +0800

    edits

--- notebook/graphs/python3/adjacency_list_directed_unweighted.py
+++ notebook/graphs/python3/adjacency_list_directed_unweighted.py
@@ -1,6 +1,6 @@
 #modified from https://www.pythonpool.com/adjacency-list-python/
 
-from dfs import dfs
+from dfs_directed import dfs
 
 adj_list = {}
 nodelist = []
@@ -31,39 +31,41 @@ def graph():
     for node in adj_list:
         print(node, " ---> ", [i for i in adj_list[node]])
 
+#https://www.pythonpool.com/adjacency-list-python/
 #Adding nodes
-#add_node(0)
-#add_node(1)
-#add_node(2)
-#add_node(3)
-#add_node(4)
+add_node(0)
+add_node(1)
+add_node(2)
+add_node(3)
+add_node(4)
 #Adding edges
-#add_edge(0,1)
-#add_edge(1,2)
-#add_edge(2,3)
-#add_edge(3,0)
-#add_edge(3,4)
-#add_edge(4,0)
+add_edge(0,1)
+add_edge(1,2)
+add_edge(2,3)
+add_edge(3,0)
+add_edge(3,4)
+add_edge(4,0)
 
 #https://www.educative.io/answers/how-to-implement-depth-first-search-in-python
-add_node('A')
-add_node('B')
-add_node('C')
-add_node('D')
-add_node('E')
-add_node('F')
-
-add_edge('A','C')
-add_edge('A','B')
-
-add_edge('B','E')
-add_edge('B','D')
-add_edge('C','F')
-add_edge('E','F')
+#add_node('A')
+#add_node('B')
+#add_node('C')
+#add_node('D')
+#add_node('E')
+#add_node('F')
+#
+#add_edge('A','C')
+#add_edge('A','B')
+#
+#add_edge('B','E')
+#add_edge('B','D')
+#add_edge('C','F')
+#add_edge('E','F')
 #Printing the graph
 graph()
 
 #Printing the adjacency list
 print(adj_list)
+
 
 print(dfs(adj_list,'A'))
